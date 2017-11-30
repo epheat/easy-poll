d0a129d5e8a6391c681deb369bb669eccb750f78
Evan Heaton <epheat07+github@gmail.com>
2017-11-30 12:08:49 -0500

    removed auth-config

--- src/auth/auth0-config.js
+++
@@ -1,6 +0,0 @@
-export const AUTH_CONFIG = {
-  clientID: '8qb9ip5acdrMtasATJcr6L6FLN1vKG31',
-  domain: 'easy-poll.auth0.com',
-  callbackUrl: 'http://138.197.109.222/callback',
-  apiUrl: 'http://easy-poll.com'
-}
